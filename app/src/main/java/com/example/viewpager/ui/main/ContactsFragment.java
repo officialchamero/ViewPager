1897be64cb4bf86fbb71dcf2c0c4d352b7e8b505
Aneesh Shahi <chameroshahi@gmail.com>
2019-11-13 10:00:25 +0545

    Initial commit

---
+++ app/src/main/java/com/example/viewpager/ui/main/ContactsFragment.java
@@ -0,0 +1,52 @@
+package com.example.viewpager.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import com.example.viewpager.R;
+
+public class ContactsFragment extends Fragment {
+
+  private static final String TAG = "Contacts";
+
+  private PageViewModel pageViewModel;
+
+  public ContactsFragment() {
+    // Required empty public constructor
+  }
+
+  /**
+   * @return A new instance of fragment ContactsFragment.
+   */
+  public static ContactsFragment newInstance() {
+    return new ContactsFragment();
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+    pageViewModel.setIndex(TAG);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View root = inflater.inflate(R.layout.fragment_main, container, false);
+    final TextView textView = root.findViewById(R.id.section_label);
+    pageViewModel.getText().observe(this, new Observer<String>() {
+      @Override
+      public void onChanged(@Nullable String s) {
+        textView.setText(s);
+      }
+    });
+    return root;
+  }
+}
